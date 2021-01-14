8a79070914a2ef1806eb3a25100a8b1453df36eb
Renan <rennanaguiar10@gmail.com>
2021-01-14 13:02:45 -0300

    finalizando

--- Backend/src/main/java/com/basis/sge/service/dominio/PerguntaEvento.java
+++ Backend/src/main/java/com/basis/sge/service/dominio/PerguntaEvento.java
@@ -3,13 +3,10 @@ package com.basis.sge.service.dominio;
 import lombok.Getter;
 import lombok.Setter;
 
-
-import javax.persistence.Column;
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToMany;
-import javax.persistence.ManyToOne;
 import javax.persistence.MapsId;
 import javax.persistence.Table;
 import java.io.Serializable;
