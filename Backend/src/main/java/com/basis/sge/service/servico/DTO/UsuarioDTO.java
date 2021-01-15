ddabafbda67584c0fda694503bd75948fee1bc45
italoFelipeDev <italofelipedev@gmail.com>
2021-01-15 17:50:09 -0300

    exceptions pergunta

--- Backend/src/main/java/com/basis/sge/service/servico/DTO/UsuarioDTO.java
+++
@@ -1,9 +0,0 @@
-package com.basis.sge.service.servico.DTO;
-
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
-public class UsuarioDTO {
-}
--- Backend/src/main/java/com/basis/sge/service/servico/mapper/UsuarioMapper.java
+++
@@ -1,7 +0,0 @@
-package com.basis.sge.service.servico.mapper;
-
-import com.basis.sge.service.dominio.Pergunta;
-import com.basis.sge.service.servico.DTO.PerguntaDTO;
-
-public interface UsuarioMapper  {
-}
