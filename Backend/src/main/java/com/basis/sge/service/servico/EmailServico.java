f5f6debec52289b72c912a8703c3cf11116f17a6
Renan <rennanaguiar10@gmail.com>
2021-01-17 12:14:26 -0300

    quase toda sprint-1 pronta

--- Backend/src/main/java/com/basis/sge/service/servico/EmailServico.java
+++ Backend/src/main/java/com/basis/sge/service/servico/EmailServico.java
@@ -1,38 +1,38 @@
-package com.basis.sge.service.servico;
-
-import com.basis.sge.service.servico.DTO.EmailDTO;
-import lombok.RequiredArgsConstructor;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMessageHelper;
-import org.springframework.messaging.MessagingException;
-import org.springframework.stereotype.Service;
-
-import javax.mail.internet.MimeMessage;
-import java.io.UnsupportedEncodingException;
-
-@Service
-@RequiredArgsConstructor
-public class EmailServico {
-
-    private static final String ERROR_TITLE = "error.title";
-
-    private final JavaMailSender javaMailSender;
-
-    public void sendMail(EmailDTO emailDTO) {
-        try {
-            MimeMessage mimeMessage = javaMailSender.createMimeMessage();
-            MimeMessageHelper message = new MimeMessageHelper(mimeMessage, false, "UTF-8");
-            message.setTo(emailDTO.getDestinatario());
-            message.setFrom("ancine-mailer@basis.com.br", "Ancine");
-            message.setSubject(emailDTO.getAssunto());
-            for (String s : emailDTO.getCopias()) {
-                message.addCc(s);
-            }
-            message.setText(emailDTO.getCorpo(), true);
-            javaMailSender.send(mimeMessage);
-        } catch (MessagingException | UnsupportedEncodingException | javax.mail.MessagingException e) {
-            throw new RuntimeException( ERROR_TITLE);
-        }
-    }
-
-}
+//package com.basis.sge.service.servico;
+//
+//import com.basis.sge.service.servico.DTO.EmailDTO;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.mail.javamail.JavaMailSender;
+//import org.springframework.mail.javamail.MimeMessageHelper;
+//import org.springframework.messaging.MessagingException;
+//import org.springframework.stereotype.Service;
+//
+//import javax.mail.internet.MimeMessage;
+//import java.io.UnsupportedEncodingException;
+//
+//@Service
+//@RequiredArgsConstructor
+//public class EmailServico {
+//
+//    private static final String ERROR_TITLE = "error.title";
+//
+//    private final JavaMailSender javaMailSender;
+//
+//    public void sendMail(EmailDTO emailDTO) {
+//        try {
+//            MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+//            MimeMessageHelper message = new MimeMessageHelper(mimeMessage, false, "UTF-8");
+//            message.setTo(emailDTO.getDestinatario());
+//            message.setFrom("ancine-mailer@basis.com.br", "Ancine");
+//            message.setSubject(emailDTO.getAssunto());
+//            for (String s : emailDTO.getCopias()) {
+//                message.addCc(s);
+//            }
+//            message.setText(emailDTO.getCorpo(), true);
+//            javaMailSender.send(mimeMessage);
+//        } catch (MessagingException | UnsupportedEncodingException | javax.mail.MessagingException e) {
+//            throw new RuntimeException( ERROR_TITLE);
+//        }
+//    }
+//
+//}
