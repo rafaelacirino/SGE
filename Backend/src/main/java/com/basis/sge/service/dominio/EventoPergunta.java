2947354d2832cc6e230e5bac68357134e61ad8f6
Renan <rennanaguiar10@gmail.com>
2021-01-19 17:52:41 -0300

    resolvido o problema de EventoPergunta

--- Backend/src/main/java/com/basis/sge/service/dominio/EventoPergunta.java
+++ Backend/src/main/java/com/basis/sge/service/dominio/EventoPergunta.java
@@ -3,7 +3,6 @@ package com.basis.sge.service.dominio;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Embeddable;
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
