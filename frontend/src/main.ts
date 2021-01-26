e9e053795dce3713a5e884c71e1053fe7e475258
Renan <rennanaguiar10@gmail.com>
2021-01-26 15:16:47 -0300

    Main sem erro

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,14 +3,13 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
-import { bootstrapSecurity } from '@nuvem/angular-base';
+
 
 if (environment.production) {
     enableProdMode();
 }
 
-bootstrapSecurity(environment.auth, () => {
-    platformBrowserDynamic()
-        .bootstrapModule(AppModule)
-        .catch(err => console.log(err));
-});
+platformBrowserDynamic()
+    .bootstrapModule(AppModule)
+    .catch(err => console.log(err));
+
