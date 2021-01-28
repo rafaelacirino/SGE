cf446380d9e7f5215dc521fa71eee5cba99d8066
italoFelipeDev <italofelipedev@gmail.com>
2021-01-28 20:27:11 -0300

    Crud usuario

--- frontend/src/app/modulos/usuario/components/formulario/formulario.component.ts
+++ frontend/src/app/modulos/usuario/components/formulario/formulario.component.ts
@@ -1,6 +1,7 @@
 import { HttpErrorResponse, HttpResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
 import { Usuario } from 'src/app/dominios/Usuario';
 import { UsuarioService } from '../../services/usuario.service';
 
@@ -10,16 +11,28 @@ import { UsuarioService } from '../../services/usuario.service';
   styleUrls: ['./formulario.component.css']
 })
 export class FormularioComponent implements OnInit {
-
+  edicao = false
   formUsuario: FormGroup
   usuario = new Usuario();
 
   constructor(
     private fb: FormBuilder,
-    private usuarioService: UsuarioService
+    private usuarioService: UsuarioService,
+    private route: ActivatedRoute
   ) { }
 
+  
+
   ngOnInit(): void {
+
+    this.route.params.subscribe(params =>{
+      if (params.id){
+        this.edicao = true;
+      }
+      
+    })
+
+
     this.formUsuario = this.fb.group({
       nome: ['',Validators.minLength(3)],
       cpf: ['', [Validators.maxLength(11), Validators.minLength(11)]],
@@ -29,19 +42,38 @@ export class FormularioComponent implements OnInit {
     })
   }
 
+  buscarUsuario(id: number) {
+    this.usuarioService.buscarUsuarioPorId(id)
+      .subscribe(usuario => this.usuario = usuario); 
+  }
+
+
+
   salvar(){
     if(this.formUsuario.invalid){
       alert("Formulario Invalido")
+      return
     }
-
-    this.usuarioService.salvarUsuario(this.usuario)
-    .subscribe(usuario => {
-      console.log('Usuario salvo', this.usuario);
-      alert('Usuario salvo')
-    }, (erro : HttpErrorResponse) => {
-      alert(erro.error.message);
+  
+    if (this.edicao) {
+      this.usuarioService.editarUsuario(this.usuario)
+        .subscribe(usuario => {
+          alert('Usuário Editado')
+        }, (erro: HttpErrorResponse) => {
+          alert(erro.error.message);
+        });
+      } else {
+      this.usuarioService.salvarUsuario(this.usuario)
+        .subscribe(usuario => {
+          alert('Usuario salvo')
+      }, (erro : HttpErrorResponse) => {
+        alert(erro.error.message);
       
-    });
+      });
+      
+    }
+        
+    
   }
 
 }
