17d8619d47854a9a19b71aabc2d634e7f29686ef
MatheusRSousa <matheus.rsousa16@gmail.com>
2021-02-01 14:27:46 -0300

    Evento de Adiministrador quase concluido

--- frontend/src/app/modulos/evento/services/evento.service.ts
+++ frontend/src/app/modulos/evento/services/evento.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Evento } from 'src/app/dominios/Evento';
+import { TipoEvento } from 'src/app/dominios/TipoEvento';
 import { environment } from 'src/environments/environment';
 
 @Injectable()
@@ -12,7 +13,14 @@ export class EventoService {
   constructor(private http: HttpClient) { }
 
   getEventos(): Observable<Evento[]>{
-
     return this.http.get<Evento[]>(`${this.url}/eventos`)
+  }
+
+  getTipoEventos(): Observable<TipoEvento[]>{
+    return this.http.get<TipoEvento[]>(`${this.url}/tipoevento`)
+  }
+
+  salvarEvento(evento: Evento): Observable<Evento>{
+    return this.http.post<Evento>(`${this.url}/eventos`, evento)
   }
 }
